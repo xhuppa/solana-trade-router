1d9ef46375d29a4985b0b22ea4341254f151da7e
agentroberto <sherwanirobert@gmail.com>
2024-06-29 12:47:48 -0400

    trade route func

--- app/config.js
+++ app/config.js
@@ -1,2 +1,3 @@
 export const SOLANA_RPC_URL = 'http://127.0.0.1:8899';
 export const PROGRAM_ID = 'Awjrxd8FQnGPYzHqTAJh7Af3NV5vxCkKiYt4PttMb5oy';
+export const TARGET_PUBLIC_KEY = 'AXtosiF7R3kN5MHb8hzbqtcNqDRtqYGcLGdqcD5Tq7jx';
--- programs/solana-trade-router/src/lib.rs
+++ programs/solana-trade-router/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
+use serum_dex::state::Market;
 
-declare_id!("Awjrxd8FQnGPYzHqTAJh7Af3NV5vxCkKiYt4PttMb5oy");
+declare_id!("7MBTsqiPjHHt5Xmaq8jjTq93r5msdVy8rxEdyGgAnV3v");
 
 #[program]
 pub mod solana_trade_router {
@@ -10,7 +11,32 @@ pub mod solana_trade_router {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn route_trade(ctx: Context<RouteTrade>) -> Result<()> {
+        let market_account_info = &ctx.accounts.market;
+        let program_id = ctx.program_id;
+
+        let market = Market::load(market_account_info, program_id, false).map_err(|e| {
+            msg!("Error loading market: {:?}", e);
+            ProgramError::Custom(0)
+        })?;
+
+        let own_address = market.own_address;
+        let vault_signer_nonce = market.vault_signer_nonce;
+
+        msg!("Market own address: {:?}", own_address);
+        msg!("Market vault signer nonce: {:?}", vault_signer_nonce);
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct RouteTrade<'info> {
+    pub market: AccountInfo<'info>,
+    #[account(signer)]
+    pub user: AccountInfo<'info>,
+}
